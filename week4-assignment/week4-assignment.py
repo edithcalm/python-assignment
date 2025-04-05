ec54a89580f75d1cad37dc865d4841eca287a85e
edithcalm <edithkaranja02@gmail.com>
2025-04-05 17:10:16 +0300

    week 4 assignment

--- week4-assignment/week4-assignment.py
+++ week4-assignment/week4-assignment.py
@@ -1,15 +1,27 @@
 try:
-	file = open("data.txt", "r")
+	file = open('data.txt', "r")
 	data = file.read()
 	file.close()
 
 	modified_data = data.upper()
-	new_file = open("modified.txt", "w")
-	new_file.write(modified_data)
+	file = open('modified.txt', "w")
+	file.write(modified_data)
 
-	new_file.close()
+	file.close()
 
 	print("The file was read, modified, and saved successfully.")
 
 except FileNotFoundError:
 	print("Oops! The file 'data.txt' was not found.")
+
+filename = input("Enter the name of the file you want to read: ")
+try:
+	file = open("filename", "r")
+	print("\n:Here is the content of the file:")
+	print(file.read())
+	file.close()
+except FileNotFoundError:
+    print("Sorry, that file does not exist. Please check the name and try again.")
+except Exception as error:
+    print(f"Something went wrong: {error}")
+
